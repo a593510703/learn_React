68231defa51703794f291533cdd4aba22d8e16e5
ksyer <593510703@qq.com>
2021-01-26 11:26:13 +0800

    添加局部state，减少Clock标签的参数

--- src/index.js
+++ src/index.js
@@ -24,11 +24,16 @@ const element = React.createElement(
 )
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
   }
@@ -36,7 +41,7 @@ class Clock extends React.Component {
 
 function tick() {
   ReactDOM.render(
-    <Clock date={new Date()} />,
+    <Clock />,
     document.getElementById('root'),
   );
 }

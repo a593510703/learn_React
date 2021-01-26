bc85b9adbc098c705dd9b71fcff35d7b2f897d47
ksyer <593510703@qq.com>
2021-01-26 14:48:20 +0800

    key仅需在兄弟节点之间唯一

--- src/index.js
+++ src/index.js
@@ -2,16 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function NumberList(props) {
-  const numbers = props.numbers;
-  const listItems = numbers.map(
-    (number) => <li key={number.toString()}>{number}</li>
+function Blog(props) {
+  const sidebar = (
+    <ul>
+      {props.posts.map((post) =>
+        <li key={post.id}>
+          {post.title}
+        </li>
+      )}
+    </ul>
   );
-  return <ul>{listItems}</ul>;
+  
+  const content = props.posts.map((post) =>
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  );
+
+  return (
+    <div>
+      {sidebar}
+      <hr />
+      {content}
+    </div>
+  );
 }
 
-const numbers = [1, 2, 3, 4, 5];
+const posts = [
+  {id: 1, title: 'Hello World', content: 'Welcome to learing react!'},
+  {id: 2, title: 'Installation', content: 'wasdasd'},
+];
+
 ReactDOM.render(
-  <NumberList numbers={numbers} />,
+  <Blog posts={posts} />,
   document.getElementById('root'),
 )

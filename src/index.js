61e84712fa356a0112878932aee570ca67be2888
ksyer <593510703@qq.com>
2021-01-26 11:23:51 +0800

    函数组件转化为class组件 -> 五步

--- src/index.js
+++ src/index.js
@@ -23,13 +23,15 @@ const element = React.createElement(
   'Hello, world!'
 )
 
-function Clock(props) {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {props.date.toLocaleTimeString()}.</h2>
-    </div>
-  );
+class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
 function tick() {

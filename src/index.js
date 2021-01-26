0c51ef7f5958840a1d94ed3be3a1e3619adc20c2
ksyer <593510703@qq.com>
2021-01-26 10:41:14 +0800

    test if

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ function formatName(user) {
   return user.firstName + ' ' + user.lastName;
 }
 
+function getGreeting(user) {
+  if (user)
+    return <h1>Hello, { formatName(user) }</h1>;
+  return <h1>Hello, Stranger.</h1>
+}
+
 const user = {
   firstName: 'Harper',
   lastName: 'Perez',
 };
 
-const element = (
-  <h1>
-    Hello, {formatName(user)}
-  </h1>
-);
-
 ReactDOM.render(
-  element,
+  getGreeting(null),
   document.getElementById('root'),
 );

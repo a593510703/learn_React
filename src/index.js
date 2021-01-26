aceb03e39d2c3f2aad7cd55eed6dbe0cfd11af4f
ksyer <593510703@qq.com>
2021-01-26 14:31:59 +0800

    封装成组件，修复key导致的warn

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const numbers = [1, 2, 3, 4, 5];
-const listItems = numbers.map(
-  (number) => <li>{number * 2}</li>
-);
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map(
+    (number) => <li key={number.toString()}>{number}</li>
+  );
+  return <ul>{listItems}</ul>;
+}
 
+const numbers = [1, 2, 3, 4, 5];
 ReactDOM.render(
-  <ul>{listItems}</ul>,
+  <NumberList numbers={numbers} />,
   document.getElementById('root'),
 )

8219c0884310f119433b4915fbdb7ac467a77a36
ksyer <593510703@qq.com>
2021-01-26 10:47:43 +0800

    createElement创建元素

--- src/index.js
+++ src/index.js
@@ -17,11 +17,10 @@ const user = {
   lastName: 'Perez',
 };
 
-const element = (
-  <div>
-    <h1>Hello!</h1>
-    <h2>Good to see you here.</h2>
-  </div>
+const element = React.createElement(
+  'h1',
+  {className: 'greeting'},
+  'Hello, world!'
 )
 
 ReactDOM.render(

50ec853a312cfdac70a09e49feb2ae9a33bb0654
ksyer <593510703@qq.com>
2021-01-26 13:58:29 +0800

    组件合并

--- src/index.js
+++ src/index.js
@@ -2,21 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Toggle extends React.Component {
-  handleClick() {
-    console.log('this is: ', this);
-  }
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
 
-  render() {
-    return (
-      <button onClick={() => this.handleClick()}>
-        Click me
-      </button>
-    );
-  }
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn)
+    return <UserGreeting />;
+  return <GuestGreeting />;
 }
 
 ReactDOM.render(
-  <Toggle />,
+  <Greeting isLoggedIn={true} />,
   document.getElementById('root'),
-);
+)

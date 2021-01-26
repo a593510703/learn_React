33e5f368edf947279e2f0f7ac3c232d195c1a4db
ksyer <593510703@qq.com>
2021-01-26 10:46:45 +0800

    JSX指定子元素

--- src/index.js
+++ src/index.js
@@ -17,7 +17,14 @@ const user = {
   lastName: 'Perez',
 };
 
+const element = (
+  <div>
+    <h1>Hello!</h1>
+    <h2>Good to see you here.</h2>
+  </div>
+)
+
 ReactDOM.render(
-  getGreeting(null),
+  element,
   document.getElementById('root'),
 );

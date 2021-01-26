d6e68116cda97f1413d4cbfeac6979ec51aabe07
ksy <593510703@qq.com>
2021-01-26 17:04:03 +0800

    仅输入摄氏温度的沸腾判断

--- src/index.js
+++ src/index.js
@@ -2,55 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Reservation extends React.Component {
+function BoilingVerdict(props) {
+  if (props.celsius >= 100)
+    return <p>The water would boil.</p>;
+  return <p>The water would not boil.</p>;
+}
+
+class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      isGoing: true,
-      numberOfGuests: 2,
-    };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {template: ''};
   }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.name === 'isGoing' ? target.checked : target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value,
-    });
+  handleChange(e) {
+    this.setState({template: e.target.value});
   }
 
   render() {
+    const temperature = this.state.template;
     return (
-      <form>
-        <label>
-          参与：
-          <input
-            name='isGoing'
-            type='checkbox'
-            checked={this.state.isGoing}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          来宾人数：
-          <input
-            name='numberOfGuests'
-            type='number'
-            checked={this.state.numberOfGuests}
-            onChange={this.handleInputChange}
-          />
-        </label>
-      </form>
+      <fieldset>
+        <legend>Enter temperature in Celsius:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+        <BoilingVerdict celsius={parseFloat(temperature)} />
+      </fieldset>
     );
   }
 }
 
 ReactDOM.render(
-  <Reservation />,
+  <Calculator />,
   document.getElementById('root'),
 );

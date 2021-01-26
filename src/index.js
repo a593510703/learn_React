4577201754f51298aca0ce25f46130a836d867ed
ksyer <593510703@qq.com>
2021-01-26 13:49:40 +0800

    class实现开关状态的Toggle组件

--- src/index.js
+++ src/index.js
@@ -2,71 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
-}
-
-function getGreeting(user) {
-  if (user)
-    return <h1>Hello, { formatName(user) }</h1>;
-  return <h1>Hello, Stranger.</h1>
-}
-
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez',
-};
-
-const element = React.createElement(
-  'h1',
-  {className: 'greeting'},
-  'Hello, world!'
-)
-
-class Clock extends React.Component {
+class Toggle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {date: new Date()};
+    this.state = {isToggleOn: true};
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      100,
+  handleClick() {
+    this.setState(
+      state => ({
+        isToggleOn: !state.isToggleOn,
+      })
     );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
   }
 
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? "ON" : "OFF"}
+      </button>
     );
   }
-
-  tick() {
-    this.setState({date: new Date()});
-  }
-}
-
-function ActionLink() {
-  function handleClick(e) {
-    e.preventDefault();
-    console.log("The bottom was clicked.");
-  }
-  return (
-    <a href="#" onClick={handleClick}>
-      Click me
-    </a>
-  );
 }
 
 ReactDOM.render(
-  ActionLink(),
+  <Toggle />,
   document.getElementById('root'),
 );

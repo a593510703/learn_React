65b7f4b0593bdd062a323e48ce82ea53d31f9531
ksy <593510703@qq.com>
2021-01-27 10:18:53 +0800

    组件继承 - 特例关系

--- src/index.js
+++ src/index.js
@@ -2,34 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Contacts() {
-  return <div className="Contacts" />;
-}
-
-function Chat() {
-  return <div className="Chat" />;
-}
-
-function SplitPane(props) {
+function FancyBorder(props) {
   return (
-    <div className="SplitPane">
-      <div className="SplitPane-left">
-        {props.left}
-      </div>
-      <div className="SplitPane-right">
-        {props.right}
-      </div>
+    <div className={'FancyBorder FancyBorder-' + props.color}>
+      {props.children}
     </div>
   );
 }
 
-function App() {
+function Dialog(props) {
   return (
-    <SplitPane left={<Contacts />} right={<Chat />} />
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">
+        {props.title}
+      </h1>
+      <p className="Dialog-message">
+        {props.message}
+      </p>
+    </FancyBorder>
+  );
+}
+
+function WelcomeDialog() {
+  return (
+    <Dialog title="Welcome" message="Thank you for visiting our spacecraft!" />
   );
 }
 
 ReactDOM.render(
-  <App />,
+  <WelcomeDialog />,
   document.getElementById('root'),
 );
